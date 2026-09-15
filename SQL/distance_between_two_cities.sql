4e686a9be83b5d83b0804c7107ae6254c28b7341
mario-gellrich-zhaw <60516564+mario-gellrich-zhaw@users.noreply.github.com>
2026-09-15 16:49:06 +0000

    Changed coordinates

--- SQL/distance_between_two_cities.sql
+++ SQL/distance_between_two_cities.sql
@@ -1,9 +1,9 @@
 SELECT ROUND(
     CAST(ST_Distance(
-        -- St. Gallen (longitude, latitude)
-        ST_GeographyFromText('SRID=4326;POINT(9.37624 47.42562)'),
-        -- Genf (longitude, latitude)
-        ST_GeographyFromText('SRID=4326;POINT(6.1466 46.20176)')
+        -- Winterthur (longitude, latitude)
+        ST_GeographyFromText('SRID=4326;POINT(8.7233 47.5034)'),
+        -- Zürich (longitude, latitude)
+        ST_GeographyFromText('SRID=4326;POINT(8.5417 47.3769)')
     ) / 1000 AS NUMERIC), -- Convert meters to kilometers
     1 -- Round to one decimal place
 ) AS distance_in_kilometers;
